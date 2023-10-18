71a41bbf4503d47f9a1a207d4f5c96885095c82f
IliasOmari <ilias.omari@student.ehb.be>
2023-10-18 11:40:42 +0200

    created a JSON file within questions for my quiz

---
+++ quiz-app/src/quiz.js
@@ -0,0 +1,36 @@
+export const quiz = {
+  questions: [
+    {
+      question: "What is the primary role of a web designer?",
+      choices: [
+        "Writing server-side code",
+        "Designing user interfaces and layouts",
+        "Managing database systems",
+        "Conducting marketing research",
+      ],
+      type: "MCQs",
+      correctAnswer: "Designing user interfaces and layouts",
+    },
+    {
+      question:
+        "Which of the following is NOT a common web design tool or software?",
+      choices: ["Phostoshop", "HTML", "Sketch", "Excel"],
+      type: "MCQs",
+      correctAnswer: "Excel",
+    },
+    {
+      question:
+        "Which coding language is commonly used by web designers for front-end development?",
+      choices: ["JavaScript", "Python", "Java", "Ruby"],
+      type: "MCQs",
+      correctAnswer: "JavaScript",
+    },
+    {
+      question:
+        "Which of the following file formats is commonly used for saving high-quality images on the web?",
+      choices: [".TXT", ".GIF", ".JPEG", ".HTML"],
+      type: "MCQs",
+      correctAnswer: ".JPEG",
+    },
+  ],
+};
